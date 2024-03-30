3576ccf055356dd065c1480c3d20d4c11ba9a828
srmartinis <sr_martinis@hotmail.com>
2024-03-30 16:50:15 -0300

    Clase 20 terminada-agregamos menu mobile

--- main.js
+++ main.js
@@ -1,12 +1,19 @@
 
 const menuEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu")
+const menuMobileIcon = document.querySelector('.menuMobileIcon');
+const menuMobile = document.querySelector('.mobile-menu')
 
 menuEmail.addEventListener("click",toggleDesktopMenu);
+menuMobileIcon.addEventListener("click",toggleMenuMobile);
 
 function toggleDesktopMenu () {
     console.log ("Click")
     desktopMenu.classList.toggle("inactive");
+}
+
+function toggleMenuMobile (){
+    menuMobile.classList.toggle('inactive');
 }
 
 
