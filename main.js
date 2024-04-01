d66e390345a66e0de3b2b1c49265a3ecaf9767c6
srmartinis <sr_martinis@hotmail.com>
2024-03-31 22:23:02 -0300

    cambios hasta clase 21

--- main.js
+++ main.js
@@ -3,17 +3,36 @@ const menuEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu")
 const menuMobileIcon = document.querySelector('.menuMobileIcon');
 const menuMobile = document.querySelector('.mobile-menu')
+const cartShopping = document.querySelector(".navbar-shopping-cart")
+const productDetail = document.querySelector(".product-detail")
 
 menuEmail.addEventListener("click",toggleDesktopMenu);
 menuMobileIcon.addEventListener("click",toggleMenuMobile);
+cartShopping.addEventListener('click',toggleProducDetail);
+
 
 function toggleDesktopMenu () {
-    console.log ("Click")
+    if (productDetail.classList.contains("product-detail")){
+        productDetail.classList.add('inactive')
+    }
     desktopMenu.classList.toggle("inactive");
 }
 
 function toggleMenuMobile (){
+    if (productDetail.classList.contains("product-detail")){
+        productDetail.classList.add('inactive')
+    }
     menuMobile.classList.toggle('inactive');
+}
+
+function toggleProducDetail (){
+    if (desktopMenu.classList.contains("desktop-menu")){
+        desktopMenu.classList.add('inactive')
+    }
+    if (menuMobile.classList.contains("mobile-menu")){
+        menuMobile.classList.add("inactive")
+    }
+    productDetail.classList.toggle('inactive');
 }
 
 
